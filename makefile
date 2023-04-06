6ab0628e8983148cf699d9f072d0f4d21f747ffd
Siddharth Bhat <siddu.druid@gmail.com>
2023-04-06 02:57:55 +0100

    add loading code

---
+++ makefile
@@ -0,0 +1,3 @@
+.PHONY: run
+run:
+	nixGL poetry run python simulation.py
--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import polyscope as ps
+import numpy as np
 
 # https://raw.githubusercontent.com/azencot/gp.fns/master/code/MESH_READER.m
 # https://en.wikipedia.org/wiki/OFF_(file_format)
@@ -6,16 +7,52 @@ import polyscope as ps
 def read_off(file):
     if 'OFF' != file.readline().strip():
         raise('Not a valid OFF header')
-    n_verts, n_faces, n_edges = tuple([int(s) for s in file.readline().strip().split(' ')])
-    verts = [[float(s) for s in file.readline().strip().split(' ')] for _ in range(n_verts)]
-    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for _ in range(n_faces)]
-    return verts, faces
+    n_vertices, n_faces, n_edges = tuple([int(s) for s in file.readline().strip().split(' ')])
+    vertices = np.asarray([[float(s) for s in file.readline().strip().split(' ')] for _ in range(n_vertices)])
+    faces = np.asarray([[int(s) for s in file.readline().strip().split(' ')][1:] for _ in range(n_faces)])
+    return vertices, faces
+
+
+
+def normv(v):
+    """L2 norm of vector"""
+    s = 0
+    for x in v: s += x
+    return np.sqrt(s)
 
 
 # https://raw.githubusercontent.com/azencot/gp.fns/master/code/MESH.m
+class Mesh:
+    def __init__(name, vertices, faces):
+        self.name = name
+        self.vertices = vertices
+        self.triangles = triangles # only triangles.
+        for t in self.triangles:
+            assert len(t) == 3
+
+        self.nv = len(self.vertices)
+        self.nf = len(self.faces)
+
+        X = self.vertices; T = self.triangles
+        NN1 = X[T[:, 0], :] - X[T[:, 1], :]
+        NN2 = X[T[:, 0], :] - X[T[:, 2], :]
+        NN = np.cross(NN1, NN2)
+        II = np.concatenate((T[:, 0], T[:, 1], T[:, 2]))
+        JJ = np.concatenate((T[:, 1], T[:, 2], T[:, 0]))
+
+
+
 
 # https://raw.githubusercontent.com/azencot/gp.fns/master/code/test_sphere_jet.m
 
 # https://raw.githubusercontent.com/azencot/gp.fns/master/code/test_sphere_rot.m
 
 # https://raw.githubusercontent.com/azencot/gp.fns/master/code/vortices_func.m
+
+if __name__ == "__main__":
+    ps.init()
+    with open("./gp.fns/data/sphere_s3.off") as f:
+      (vertices, faces) = read_off(f)
+      ps.register_surface_mesh("s3", vertices, faces, smooth_shade=True)
+    ps.show()
+
